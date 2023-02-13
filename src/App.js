f7163d5097fb10322446c0067fdba8808d9fd929
Subramanya K S <subramanyaks2205@gmail.com>
2023-02-13 18:10:48 +0530

    updated useReduce Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
+import UseReduceHook from './components/UseReduce/UseReduceHook';
 import UseStateHook from './components/UseState/UseStateHook';
 
 function App() {
   return (
     <div className="App">
       <UseStateHook/>
+      <UseReduceHook/>
     </div>
   );
 }
